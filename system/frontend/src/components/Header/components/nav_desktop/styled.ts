d37cc85d20f0fecb0afa17f68e89366623e84030
LuizFernandoDeveloper <luiz.fernando.developer@outlook.com>
2024-04-20 15:02:21 -0300

    Committing local changes before reverting

--- system/frontend/src/components/Header/components/nav_desktop/styled.ts
+++
@@ -1,8 +0,0 @@
-import { BreakPoints, WebSystemColor } from '../../../../main-styles';
-import styled, { keyframes } from 'styled-components';
-import {
-  BsPersonWorkspaceIcon,
-  CiUserIcon,
-  FaCartShoppingIcon,
-} from '../../../../main-styles';
-import { Link } from 'react-router-dom';
